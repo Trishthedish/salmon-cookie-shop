59947be85ca456af4770e70c756822af94fde582
Trish Griego <pmgriego@gmail.com>
2015-12-13 21:23:46 -0800

    Made changes to look of home page. More changes necessary.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+//kept getting a reffernce error for $ is not defined. 
+// $(window).load(function(){
+// $('img.fade').hide();
+// var dg_H = $(window).height();
+// var dg_W = $(window).width();
+// $('#wrap').css({'height':dg_H,'width':dg_W});
+// function anim() {
+//     $("#wrap img.fade").first().appendTo('#wrap').fadeOut(1500);
+//     $("#wrap img").first().fadeIn(1500);
+//     setTimeout(anim, 3000);
+// }
+// anim();})
+// $(window).resize(function(){window.location.href=window.location.href})
