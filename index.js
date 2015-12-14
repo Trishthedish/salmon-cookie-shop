faa20cdb51b511d31ce424c573fb98e9a83ed61a
Trish Griego <pmgriego@gmail.com>
2015-12-13 23:11:10 -0800

    After many attempts finally got my css transitions to work. I used jquery and added more layers to css style sheet. Much improved but definitely not the best of what I can do.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 
 
-//kept getting a reffernce error for $ is not defined. 
-// $(window).load(function(){
-// $('img.fade').hide();
-// var dg_H = $(window).height();
-// var dg_W = $(window).width();
-// $('#wrap').css({'height':dg_H,'width':dg_W});
-// function anim() {
-//     $("#wrap img.fade").first().appendTo('#wrap').fadeOut(1500);
-//     $("#wrap img").first().fadeIn(1500);
-//     setTimeout(anim, 3000);
-// }
-// anim();})
-// $(window).resize(function(){window.location.href=window.location.href})
+$(window).load(function(){
+$('img.fade').hide();
+var dg_H = $(window).height();
+var dg_W = $(window).width();
+$('#wrap').css({'height':dg_H,'width':dg_W});
+function anim() {
+    $("#wrap img.fade").first().appendTo('#wrap').fadeOut(1500);
+    $("#wrap img").first().fadeIn(1500);
+    setTimeout(anim, 4000);
+}
+anim();})
+$(window).resize(function(){window.location.href=window.location.href})
+
+ //keep getting message: "uncaught reference error". Spent hours tryihng to figure out why this wasn't working. Only to realize it was the placement of my js script tags. I needed to place my script above:<script src="index.js"...
+
+//playing with numbers orignally top down: 1500, 1500, 3000.
+
+// found this article to help me achieve this: http://www.css-jquery-design.com/2013/05/jquery-animate-full-page-multiple-background-image-fade-in-out-effect/
